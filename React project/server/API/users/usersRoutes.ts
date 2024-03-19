46282e3bc9e1518f3869e2aacf2c1e762617c31d
moria-avraham <128312303+moria-avraham@users.noreply.github.com>
2024-03-19 16:26:00 +0200

    middleware

--- React project/server/API/users/usersRoutes.ts
+++ React project/server/API/users/usersRoutes.ts
@@ -1,15 +1,16 @@
 import express from "express";
 import { createUser, deleteUserById, getAllUsers, getUserFromToken, logIn, updateUserById } from "./usersCont";
+import { isAdmin } from "./Middleware";
 
 const router = express.Router()
 
 router
-    .get("/get-all-users", getAllUsers)
+    .get("/get-all-users", isAdmin, getAllUsers)
     .get("/get-from-Token", getUserFromToken)
     .post("/register", createUser)
     .post("/logIn", logIn)
-    .delete("/delete-user/:userID", deleteUserById)
-    .patch("/update-user/:userID", updateUserById)
+    .delete("/delete-user/:userID", isAdmin, deleteUserById)
+    .patch("/update-user/:userID", isAdmin, updateUserById)
 
 
 
