bd588a5661499f7b7744cfd008e4929252a91133
moria-avraham <128312303+moria-avraham@users.noreply.github.com>
2024-03-20 19:38:15 +0200

    ger user from token

--- React project/client/shoes/src/features/userAPI.ts
+++ React project/client/shoes/src/features/userAPI.ts
@@ -1,12 +1,28 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const getUserApi = createAsyncThunk('get-user', async () => {
+export const getUserApi = createAsyncThunk('get-user', async (_, thunkApi) => {
     try {
         const { data } = await axios.get('/api/users/get-from-Token')
         if (!data) throw new Error("no data on function getUserApi")
+        // if (!data.user || !data.user.email) {
+        //     const result = {
+        //         user_id: 0,
+        //         user_full_name: "",
+        //         user_email: "",
+        //         user_phone: 0,
+        //         role: ""
+        //     }
+        //     return result;
+        // } else {
+
         return data.user
-    } catch (error) {
-        console.error(error)
+        // }
+    } catch (error: any) {
+        console.error(error);
+        return thunkApi.rejectWithValue({
+            error: error.message,
+            message: error.message,
+        });
     }
 }) 
